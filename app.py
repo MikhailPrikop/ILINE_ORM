eb33fd3536990dcbb00bfc9508baa64271fb4833
Михаил Васильевич Прикоп <prikop_mv@corp.istp.ru>
2026-02-15 05:58:51 +0000

    Доработка

--- app.py
+++ app.py
@@ -1,60 +1,60 @@
 #!/usr/bin/env python
 
 import os
-from flask import Flask
-from flask import render_template
-from flask import request
-from flask import redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from database import db, Employee
-
+import threading
 import webbrowser
-webbrowser.open('http://127.0.0.1:5000')
+from flask import Flask, render_template, request, redirect, url_for
+from sqlalchemy.orm import aliased
+from sqlalchemy import asc, desc
+from database import db, Employee
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI", 'postgresql://postgres@localhost/postgres')
-
 db.init_app(app)
 
-### вызов всей таблицы с возможностью сортировки
+### основной запрос
 @app.route("/")
 def employees_list():
-    #основной запрос
     query = Employee.query
-
 
     #поиск
     search_term = request.args.get('search', '').strip()
     if search_term:
         query = query.filter(Employee.full_name.ilike(f'%{search_term}%'))
 
-    sort_field = request.args.get('sort','full_name')
+    sort_field = request.args.get('sort', 'id')
     sort_order = request.args.get('order', 'asc')
 
-    #поля сортировки
+    #cортировка
     valid_fields = ['id', 'full_name', 'position', 'date_employment', 'salary', 'manager']
-
     if sort_field not in valid_fields:
-        sort_field = 'full_name'
-    #выбор колонки сортировки
-    order_col = getattr(Employee, sort_field)
+        sort_field = 'id'
 
-    #направление сортировки
-    if sort_order == 'desc':
-        order_col = order_col.desc()
+
+    if sort_field == 'manager':
+        manager_alias = aliased(Employee)
+        query = query.outerjoin(manager_alias, Employee.manager)
+        if sort_order == 'asc':
+            query = query.order_by(asc(manager_alias.full_name))
+        else:
+            query = query.order_by(desc(manager_alias.full_name))
     else:
-        order_col = order_col.asc()
+        column = getattr(Employee, sort_field)
+        if sort_order == 'asc':
+            query = query.order_by(asc(column))
+        else:
+            query = query.order_by(desc(column))
 
-    query = query.order_by(order_col)
     employees = query.all()
 
-
-
     return render_template(
-        'employees.html', employees=query,
-        sort_field=sort_field, sort_order=sort_order)
-
-### Изменение начальника
+        'employees.html',
+        employees=employees,
+        sort_field=sort_field,
+        sort_order=sort_order,
+        search_term=search_term
+    )
+### выбор начальника
 @app.route("/employee/<int:id>/edit_manager", methods=['GET', 'POST'])
 def edit_manager(id):
     employee = Employee.query.get_or_404(id)
@@ -62,7 +62,6 @@ def edit_manager(id):
     if request.method == 'POST':
         new_manager_id = request.form.get('manager_id')
         if new_manager_id:
-            # проверка существования сотрудника
             manager = Employee.query.get(new_manager_id)
             if manager is None:
                 return "Начальник не найден", 400
@@ -75,8 +74,11 @@ def edit_manager(id):
         db.session.commit()
         return redirect(url_for('employees_list'))
 
-    employees = Employee.query.filter(Employee.id != id).all()
-    return render_template('edit_manager.html', employee=employee, employees=employees)
+    #djpvj;yst yfxfkmybrb
+    possible_managers = Employee.query.filter(Employee.id != id).all()
+    return render_template('edit_manager.html', employee=employee, employees=possible_managers)
 
 if __name__ == '__main__':
+    # Открываем браузер через 1.25 секунды после запуска сервера
+    threading.Timer(1.25, lambda: webbrowser.open('http://127.0.0.1:5000')).start()
     app.run(debug=True)
